57f72fc0c68b1ff4353d2616366d90aa2207bf4a
alfuramadhan23692370 <alfuramad@gmail.com>
2026-06-08 22:04:38 +0700

    fix: mengatasi read-only file system di vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
-// Mengarahkan Vercel untuk mengeksekusi file index utama milik Laravel
+// Buat folder cache otomatis di direktori /tmp milik Vercel
+use Illuminate\Foundation\Application;
+
+if (!file_exists('/tmp/bootstrap/cache')) {
+    mkdir('/tmp/bootstrap/cache', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
