d5b5e679b0b0f665ac647b9de0deea98bf1647df
alfuramadhan23692370 <alfuramad@gmail.com>
2026-06-08 22:14:08 +0700

    fix: relokasi package manifest ke folder tmp vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,21 @@
 <?php
 
-// Mengarahkan Vercel untuk membaca file index.php bawaan Laravel yang ada di folder public
+// Pastikan folder bootstrap cache tiruan ada di /tmp dan bisa ditulis
+if (isset($_ENV['VERCEL_URL'])) {
+    if (!file_exists('/tmp/bootstrap/cache')) {
+        mkdir('/tmp/bootstrap/cache', 0755, true);
+    }
+    
+    // Salin file manifes bawaan jika ada agar Laravel tidak mencoba menulis dari awal
+    $originalCache = __DIR__ . '/../bootstrap/cache';
+    if (file_exists($originalCache)) {
+        foreach (scandir($originalCache) as $file) {
+            if ($file !== '.' && $file !== '..') {
+                copy("$originalCache/$file", "/tmp/bootstrap/cache/$file");
+            }
+        }
+    }
+}
+
+// Mengarahkan Vercel untuk membaca file index.php bawaan Laravel
 require __DIR__ . '/../public/index.php';
